e8d5a04848ce2b00685828409d87ca81bb5c3728
Ganesh Gopu <122423846+Ganesh630@users.noreply.github.com>
2023-06-01 07:35:33 +0530

    ganesh.js

--- ganesh.js
+++ ganesh.js
@@ -1,1 +1,6 @@
 
+function manipulateStyles(){
+    console.log("this  is ganesh")
+    document.getElementById("headingElement").textContent = "Ganesh Naidu >gnshgopu@gmail.com";
+    document.getElementById("headingElement").style.color = "green";
+}
